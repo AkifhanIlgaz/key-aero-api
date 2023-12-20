69559cba9aaf45e957194b4a7636aece196741b4
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-12-20 10:40:38 +0300

    start server

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 
 	"github.com/AkifhanIlgaz/key-aero-api/cfg"
+	"github.com/AkifhanIlgaz/key-aero-api/controllers"
 	"github.com/AkifhanIlgaz/key-aero-api/db"
+	"github.com/AkifhanIlgaz/key-aero-api/routes"
+	"github.com/AkifhanIlgaz/key-aero-api/services"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	ctx := context.TODO()
+
 	config, err := cfg.LoadConfig(".")
 	if err != nil {
 		log.Fatal("Could not read environment variables", err)
@@ -20,4 +29,31 @@ func main() {
 
 	defer databases.Postgres.Close()
 	defer databases.Redis.Close()
+
+	server := gin.Default()
+	setCors(server)
+
+	userService := services.NewUserService(ctx, databases.Postgres)
+	tokenService := services.NewTokenService(ctx, config, databases.Redis)
+
+	authController := controllers.NewAuthController(config, userService, tokenService)
+
+	authRouteController := routes.NewAuthRouteController(authController)
+
+	router := server.Group("/api")
+	router.GET("/health-checker", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": "API is healthy"})
+	})
+
+	authRouteController.AuthRoute(router)
+
+	log.Fatal(server.Run(":" + config.Port))
+}
+
+func setCors(server *gin.Engine) {
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:8000", "http://localhost:3000"}
+	corsConfig.AllowCredentials = true
+
+	server.Use(cors.New(corsConfig))
 }
